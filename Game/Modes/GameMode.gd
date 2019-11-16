e077b7e940466419cbefb3131416843e10a46200
Jérémie Taboada <jeremtab@MacBook-Pro-de-Jeremie.local>
2019-11-16 17:21:26 +0100

    Added signal for colorblock in gamemode

--- Game/Modes/GameMode.gd
+++ Game/Modes/GameMode.gd
@@ -3,6 +3,8 @@ extends Control
 onready var Game = get_node("/root/Game")
 onready var MapSlot: Node2D = $MapSlot
 
+signal item_color_switch_toggle
+
 # options available in _ready
 export var options = {}
 
@@ -10,3 +12,7 @@ export var options = {}
 # @abstract
 func start():
 	pass
+
+# toggle on on/off color switch for the given color
+func item_color_switch_toggle(isOn: bool, color: int):
+	emit_signal("item_color_switch_toggle", isOn, color)
