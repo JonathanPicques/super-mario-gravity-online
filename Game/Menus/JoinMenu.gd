73a5185a2ba53777d49c6bd770044c0fa0fc591f
Jérémie Taboada <jeremtab@MacBook-Pro-de-Jeremie.local>
2019-11-18 14:17:45 +0100

    Fixed joinmenu

--- Game/Menus/JoinMenu.gd
+++ Game/Menus/JoinMenu.gd
@@ -1,7 +1,13 @@
 extends Control
 
 onready var Game = get_node("/root/Game")
+onready var RoomInput = $MarginContainer/VBoxContainer/RoomCodeInput
+
+func _ready():
+	RoomInput.grab_focus()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Game.goto_main_menu_scene()
+	if Input.is_action_just_pressed("ui_accept"):
+		print("Send code ", RoomInput.text)
