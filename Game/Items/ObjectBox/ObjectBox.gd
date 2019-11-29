53a38bfec648894ddf0bd9f9daa435042738b52f
jeremtab <taboada.jeremie@gmail.com>
2019-11-29 18:48:31 +0100

    Add effect on object collect

--- Game/Items/ObjectBox/ObjectBox.gd
+++ Game/Items/ObjectBox/ObjectBox.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+onready var Effect = preload("res://Game/Items/ObjectBox/CollectedEffect.tscn")
+
 func _on_Area2D_body_entered(body):
 	if body.current_object == null:
 		body.get_object()
+		var effect_node = Effect.instance()
+		effect_node.position = $EffectPosition.global_position
+		get_parent().add_child(effect_node)
 		queue_free()
