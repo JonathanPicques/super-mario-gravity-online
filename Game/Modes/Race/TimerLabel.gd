f6616b723a9746d6975f70191306c1192f2ef772
jeremtab <taboada.jeremie@gmail.com>
2019-12-06 17:49:23 +0100

    Added timer

---
+++ Game/Modes/Race/TimerLabel.gd
@@ -0,0 +1,24 @@
+extends Label
+
+var ms = 0
+var s = 0
+var m = 0
+
+func _process(delta):
+	if ms > 9:
+		s += 1
+		ms = 0
+	
+	if s > 59:
+		m += 1
+		s = 0
+	
+	text = "%s:%s:%s" % [
+		str(m) if m > 9 else "0" + str(m),
+		str(s) if s > 9 else "0" + str(s),
+		str(ms)
+	]
+
+
+func _on_MSTimer_timeout():
+	ms += 1
