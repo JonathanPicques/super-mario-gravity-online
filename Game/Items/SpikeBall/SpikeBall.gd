98fdb5bfc28393dfb557b1f122748ed90e11a7ee
jeremtab <taboada.jeremie@gmail.com>
2019-12-09 00:40:57 +0100

    Added spikeballs

---
+++ Game/Items/SpikeBall/SpikeBall.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+export var length := 8
+export var rotation_speed = 2
+
+onready var Chain =  preload("res://Game/Items/SpikeBall/Chain.tscn")
+onready var ball_node = $Ball
+
+var chain_nodes = []
+
+const chain_space = 8
+
+var current_direction = 1
+
+func _ready():
+	for i in range(0, length):
+		var chain_node = Chain.instance()
+		chain_node.position = Vector2(0, i * chain_space)
+		chain_nodes.append(chain_node)
+		add_child(chain_node)
+	ball_node.position = Vector2(0, length * chain_space)
+
+func _process(delta):
+	for chain_node in chain_nodes:
+		chain_node.rotate(-delta * rotation_speed * current_direction)
+	ball_node.rotate(-delta * rotation_speed * current_direction)
+	rotate(delta * rotation_speed * current_direction)
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("PlayerNode"):
+		body.apply_death(position + ball_node.position)
+	else:
+		current_direction *= -1
