9dd0ce97eea0481518ef81195bf5622460d4df45
jeremtab <taboada.jeremie@gmail.com>
2019-12-09 00:53:16 +0100

    Fixed Spikeball position

--- Game/Items/SpikeBall/SpikeBall.gd
+++ Game/Items/SpikeBall/SpikeBall.gd
@@ -4,7 +4,9 @@ export var length := 8
 export var rotation_speed = 2
 
 onready var Chain =  preload("res://Game/Items/SpikeBall/Chain.tscn")
-onready var ball_node = $Ball
+
+onready var offset_node = $Offset
+onready var ball_node = $Offset/Ball
 
 var chain_nodes = []
 
@@ -13,18 +15,18 @@ const chain_space = 8
 var current_direction = 1
 
 func _ready():
-	for i in range(0, length):
+	for i in range(0, length - 1):
 		var chain_node = Chain.instance()
 		chain_node.position = Vector2(0, i * chain_space)
 		chain_nodes.append(chain_node)
-		add_child(chain_node)
+		offset_node.add_child(chain_node)
 	ball_node.position = Vector2(0, length * chain_space)
 
 func _process(delta):
 	for chain_node in chain_nodes:
 		chain_node.rotate(-delta * rotation_speed * current_direction)
 	ball_node.rotate(-delta * rotation_speed * current_direction)
-	rotate(delta * rotation_speed * current_direction)
+	offset_node.rotate(delta * rotation_speed * current_direction)
 
 
 func _on_Area2D_body_entered(body):
