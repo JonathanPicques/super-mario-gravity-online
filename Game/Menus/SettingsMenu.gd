df097c9baeaf71f1c6e934d52a34bddbb81181c2
jeremtab <taboada.jeremie@gmail.com>
2019-12-22 01:33:45 +0100

    Fixeed minimap settings

--- Game/Menus/SettingsMenu.gd
+++ Game/Menus/SettingsMenu.gd
@@ -3,6 +3,7 @@ extends Navigation2D
 var current_row = 0
 
 const keys = ["music", "sfx", "minimap"]
+const types = ["range", "range", "toggle"]
 
 onready var labels = [
 	$GUI/Row1Label,
@@ -10,7 +11,7 @@ onready var labels = [
 	$GUI/Row3Label
 ]
 
-onready var toggles = [
+onready var values = [
 	$GUI/Row1Toggle,
 	$GUI/Row2Toggle,
 	$GUI/Row3Toggle
@@ -19,9 +20,12 @@ onready var toggles = [
 func _ready():
 	AudioManager.play_music("res://Game/Menus/Musics/Awkward-Princesss-Day-Out.ogg")
 	refresh_labels_color()
-	for i in range(0, toggles.size()):
-		toggles[i].text = str(SettingsManager.values[keys[i]])
-
+	for i in range(0, values.size()):
+		if types[i] == "range":
+			values[i].text = str(SettingsManager.values[keys[i]])
+		elif types[i] == "toggle":
+			values[i].text = "On" if SettingsManager.values[keys[i]] else "Off"
+			
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Game.goto_home_menu_scene()
@@ -32,26 +36,29 @@ func _process(delta):
 		current_row += 1
 		refresh_labels_color()
 	if Input.is_action_just_pressed("ui_accept"):
-		toggle_label()	
+		update_value()	
 
 # @impure
 func refresh_labels_color():
 	var i = 0
 	for i in range(0, labels.size()):
 		labels[i].set("custom_colors/font_color", Color("#1B192C" if i != current_row else "#2CB274"))
-		toggles[i].set("custom_colors/font_color", Color("#1B192C" if i != current_row else "#2CB274"))
+		values[i].set("custom_colors/font_color", Color("#1B192C" if i != current_row else "#2CB274"))
 		i += 1
 
 # @impure
-func toggle_label():
-	var value = int(toggles[current_row].text)
-	var new_value = value + 1 if value < 5 else 0
-	toggles[current_row].text = str(new_value)
+func update_value():
+	var new_value
+	if types[current_row] == "range":
+		var value = int(values[current_row].text)
+		new_value = value + 1 if value < 5 else 0
+		values[current_row].text = str(new_value)
+	elif types[current_row] == "toggle":
+		var value = values[current_row].text == "On"
+		new_value = !value
+		values[current_row].text = "On" if new_value else "Off"
 	SettingsManager.values[keys[current_row]] = new_value
-	if keys[current_row] == "sfx":
-		pass
 	SettingsManager.apply_settings()
-
 
 func _on_Map_tree_exited():
 	SettingsManager.save_settings()
