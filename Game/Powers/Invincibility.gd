c556d5c00e0016807f8eb039bbc08149906538f6
jeremtab <taboada.jeremie@gmail.com>
2020-01-10 09:41:46 +0100

    Handle invincibility with masking

--- Game/Powers/Invincibility.gd
+++ Game/Powers/Invincibility.gd
@@ -3,11 +3,16 @@ class_name InvincibilityPowerNodeNode
 
 onready var InvincibilityTimer: Timer = $Timer
 
+const INVINCIBILITY_FRAMES = 18.0 # TODO: get that from sprite
+
 # @impure
 # @override
 func start_power():
 	player_node.is_invincible += 1
 	player_node.PlayerInvincibilityEffect.visible = true
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_texture", player_node.PlayerSprite.texture)
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_offset", INVINCIBILITY_FRAMES / player_node.PlayerSprite.hframes)
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_flip", 0 if player_node.direction == 1 else 1)
 	SkinManager.replace_skin(player_node.PlayerSprite, player_node.player.skin_id, true)
 	InvincibilityTimer.start()
 
@@ -15,6 +20,9 @@ func start_power():
 # @override
 func process_power(delta: float):
 	set_hud_progress(InvincibilityTimer.time_left / InvincibilityTimer.wait_time)
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_texture", player_node.PlayerSprite.texture)
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_offset", INVINCIBILITY_FRAMES / player_node.PlayerSprite.hframes)
+	player_node.PlayerInvincibilityEffect.material.set_shader_param("mask_flip", 0 if player_node.direction == 1 else 1)
 	return InvincibilityTimer.is_stopped()
 
 # @impure
