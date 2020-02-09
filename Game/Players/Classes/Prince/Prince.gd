eca88ccc143369806cb7a169ed1aefbc6188dd4a
jeremtab <taboada.jeremie@gmail.com>
2020-02-09 19:33:25 +0100

    Added new prince

--- Game/Players/Classes/Prince/Prince.gd
+++ Game/Players/Classes/Prince/Prince.gd
@@ -8,9 +8,3 @@ func _ready():
 	WalljumpSFX = preload("res://Game/Players/Classes/Prince/Sounds/jump.ogg")
 	Step_01_SFX = preload("res://Game/Players/Classes/Prince/Sounds/step_01.ogg")
 	Step_02_SFX = preload("res://Game/Players/Classes/Prince/Sounds/step_02.ogg")
-	
-	has_trail += 1
-	is_invincible += 1
-	speed_multiplier = 2.0
-	PlayerLifetimeTimer.wait_time = 8.0
-	PlayerLifetimeTimer.start()
