3cc677df5dddab0382e8f78c9ef1a712e207db0b
JonathanPicques <jonathan.picques@gmail.com>
2020-03-18 02:30:41 +0100

    fix(player): fix player z index in water
    Closes #113

--- Game/Players/States/enter_swim.gd
+++ Game/Players/States/enter_swim.gd
@@ -2,8 +2,12 @@ extends FiniteStateMachineStateNode
 
 func start_state():
 	context.set_animation("swim")
+	context.PlayerSprite.z_index = -1
 	context.jumps_remaining = context.MAX_JUMPS
 	$Tween.stop_all()
 	$Tween.interpolate_property(context, "velocity:y", null, 0.0, 0.3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 	$Tween.start()
 	return fsm.states.swim
+
+func finish_state():
+	context.PlayerSprite.z_index = 0
--- Game/Players/States/swim.gd
+++ Game/Players/States/swim.gd
@@ -2,11 +2,15 @@ extends FiniteStateMachineStateNode
 
 func start_state():
 	context.set_animation("swim")
+	context.PlayerSprite.z_index = -1
 
 func process_state(delta: float):
 	context.disable_snap = 1.0
 	context.handle_swim_move(delta, context.SWIM_MAX_SPEED, context.SWIM_ACCELERATION, context.SWIM_DECELERATION)
-	context.handle_last_safe_position()
 	context.handle_direction()
+	context.handle_last_safe_position()
 	if not context.is_in_water():
 		return fsm.states.jump
+
+func finish_state():
+	context.PlayerSprite.z_index = 0
