41a76c8665a98d193dbb0f5c1a88eeb7c9cacc19
jeremtab <taboada.jeremie@gmail.com>
2020-04-01 20:39:17 +0200

    Load maps from jsong

--- Game/Menus/YourMapsMenu.gd
+++ Game/Menus/YourMapsMenu.gd
@@ -32,15 +32,15 @@ const maps := [
 		"preview": "res://Game/Maps/Textures/SpikeCorridorPreview.png",
 		"difficulty": "hard",
 		"map_scene_path": "res://Game/Maps/SpikesCorridor.tscn",
-	},
-	{
-		"title": "Custom Map",
-		"score": 42,
-		"author": "rootkernel & the world",
-		"preview": "res://Game/Maps/Textures/SpikeCorridorPreview.png",
-		"difficulty": "inferno",
-		"map_scene_path": "res://Game/Maps/SpikesCorridor.tscn",
 	}
+]
+
+onready var MapButtons := [
+	$GUI/MapButton1,
+	$GUI/MapButton2,
+	$GUI/MapButton3,
+	$GUI/MapButton4,
+	$GUI/MapButton5
 ]
 
 # @impure
@@ -49,6 +49,13 @@ func _ready():
 	AudioManager.play_music("res://Game/Menus/Musics/Awkward-Princesss-Day-Out.ogg")
 	# GUI
 	$GUI/RandomMap.grab_focus()
+	for i in range(0, MapButtons.size() - 1):
+		if i < maps.size():
+			MapButtons[i].get_node("Label").text = maps[i].title
+			MapButtons[i].get_node("MapPreview").texture = load(maps[i].preview)
+		else:
+			MapButtons[i].get_node("Label").text = ""
+			MapButtons[i].get_node("MapPreview").texture = null
 
 # @impure
 func _process(delta):
