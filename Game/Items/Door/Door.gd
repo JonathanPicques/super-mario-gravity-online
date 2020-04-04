7d9e97eb0a123ebe06f4845d32bf383c9926f7a6
jeremtab <taboada.jeremie@gmail.com>
2020-04-04 19:04:40 +0200

    Fixed some icons

--- Game/Items/Door/Door.gd
+++ Game/Items/Door/Door.gd
@@ -26,6 +26,7 @@ func load_map_data(item_data):
 	door_to_node_path = item_data["door_to"]
 	if door_to_node_path:
 		door_to_node = get_node(door_to_node_path)
+		print("door path = ", door_to_node_path, " door = ", door_to_node)
 
 func quadtree_item_rect() -> Rect2:
 	return Rect2(position, Vector2(32, 48))
