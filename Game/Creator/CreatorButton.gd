bdd57d3557ef04112390cfe8eea60fb0a2e34b28
jeremtab <taboada.jeremie@gmail.com>
2020-04-06 15:44:19 +0200

    Fixed wording and buttons scale in creator

--- Game/Creator/CreatorButton.gd
+++ Game/Creator/CreatorButton.gd
@@ -2,3 +2,40 @@ extends TextureButton
 
 func quadtree_item_rect() -> Rect2:
 	return Rect2(rect_position, rect_size)
+
+var previous_position := Vector2()
+
+func _on_SmallButton_focus_entered():
+	var scale = Vector2(1.1, 1.1)
+	var button_size = self.get_size()
+	var button_pos = self.rect_global_position
+	previous_position = button_pos
+	self.set_scale(scale)
+	self.set_global_position(button_pos + button_size * (Vector2.ONE - scale) / 2)
+
+
+func _on_SmallButton_focus_exited():
+	var scale = Vector2(1, 1)
+	var button_size = self.get_size()
+	var button_pos = self.rect_global_position
+	
+	self.set_scale(scale)
+	self.set_global_position(previous_position)
+
+
+func _on_BigButton_focus_entered():
+	var scale = Vector2(1.1, 1.1)
+	var button_size = self.get_size()
+	var button_pos = self.rect_global_position
+	previous_position = button_pos
+	self.set_scale(scale)
+	self.set_global_position(button_pos + button_size * (Vector2.ONE - scale) / 2)
+
+
+func _on_BigButton_focus_exited():
+	var scale = Vector2(1, 1)
+	var button_size = self.get_size()
+	var button_pos = self.rect_global_position
+	
+	self.set_scale(scale)
+	self.set_global_position(previous_position)
