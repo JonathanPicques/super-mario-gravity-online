e0cf4912a2929add352fcc8bda84a942a21786a7
jeremtab <taboada.jeremie@gmail.com>
2020-05-07 23:48:27 +0200

    Added new cage

--- Game/Items/StartCage/StartCage.gd
+++ Game/Items/StartCage/StartCage.gd
@@ -16,8 +16,8 @@ func load_map_data(item_data: Dictionary):
 	position.y = item_data["position"][1]
 
 func quadtree_item_rect() -> Rect2:
-	return Rect2(position, Vector2(96, 144))
+	return Rect2(position, Vector2(96, 96))
 
 func _on_Timer_timeout():
 	$AnimatedSprite.play("open")
-	$Door/CollisionShape2D.position.y = 62
+	$Door.queue_free()
